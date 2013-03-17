e077a0a225ef831fdd24c5900b7996e6400f6340
Will Scott <willscott@gmail.com>
2013-03-17 16:32:30 -0700

    Update client to handle repeated images.

--- client/js/instacdn.js
+++ client/js/instacdn.js
@@ -1,28 +1,26 @@
-var instacdn_cache = {};
 var outstanding = {};
 
-freedom.on('load', rewrite_images);
-//window.addEventListener('load', rewrite_images, true);
-
-function rewrite_images() {
-  // Rewrite all img tags with instacdn_src attribute
+freedom.on('load', function() {
+  // Rewrite all img tags with data-src set.
   var images = document.getElementsByTagName('img');
   var urls = [];
   for (var i = 0; i < images.length; i++) {
     var url = images[i].getAttribute("data-src");
-    if (url != null) {
-      outstanding[url] = images[i];
+    if (url != null && typeof outstanding[url] === 'undefined') {
+      outstanding[url] = [images[i]];
       urls.push(url);
+    } else if (url != null) {
+      outstanding[url].push(images[i]);
     }
   }
   freedom.emit('fetch', urls);
-}
+});
 
 freedom.on('resource', function(rsrc) {
   var url = rsrc['url'];
-  var el = outstanding[url];
-  if (el) {
-    el.src = rsrc['src'];
-    delete outstanding[url];
+  var els = outstanding[url];
+  for (var i = 0; els && i < els.length; i++) {
+    els[i].src = rsrc['src'];
   }
+  delete outstanding[url];
 });
