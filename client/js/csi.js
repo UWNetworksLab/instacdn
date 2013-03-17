6b11e2def6463652d486a3fb49c63a700fbe79d1
Will Scott <willscott@gmail.com>
2013-03-17 16:58:37 -0700

    update for load times > 1second

--- client/js/csi.js
+++ client/js/csi.js
@@ -4,7 +4,10 @@ var timer = function() {
   end = window.performance.now();
 }
 window.addEventListener('DOMSubtreeModified', timer);
-window.setTimeout(function() {
-  window.removeEventListener('DOMSubtreeModified', timer);
-  document.body.appendChild(document.createTextNode("Load time: " + (end - start)));
+var timeID = window.setInterval(function() {
+  if (end >= 0) {
+    window.removeEventListener('DOMSubtreeModified', timer);
+    document.body.appendChild(document.createTextNode("Load time: " + (end - start)));
+  }
+  window.clearInterval(timeID);
 }, 1000);
