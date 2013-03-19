abe3486ff45e8e38f9a7c10b8a201ce63d30f3c7
Will Scott <willscott@gmail.com>
2013-03-19 15:17:51 -0700

    update unfreedom

--- client/js/unfreedom.js
+++ client/js/unfreedom.js
@@ -1,3 +1,17 @@
+var fdom = {
+  apis: {
+    register: function(a, b) {
+      freedom.tran = new b({
+        postMessage: function(m) {
+          if (m.action == 'event') {
+            fdom.tp.emit(m.type, m.value);
+          }
+        }
+      });
+    }
+  }
+}
+
 var freedom = {
   identity: function() {
     return {
@@ -14,8 +28,41 @@ var freedom = {
           ret.cb(x);
         });
         return ret;
+      },
+      send: function(a) {
+        var ret = { done: function(c) { ret.cb = c; }};
+        freedom.ident.send(a, function(x) {
+          ret.cb(x);
+        });
+        return ret;
       }
     }
+  },
+  'core.transport': function() {
+    var x = {
+      on: function(a, f) {
+        freedom['core.transport']['on' + a] = f;
+      },
+      emit: function(a, b) {
+        freedom['core.transport']['on' + a](b);
+      },
+      create: function() {
+        var ret = { done: function(c) { ret.cb = c; }};
+        freedom.tran.create(function(x) {
+          ret.cb(x);
+        });
+        return ret;
+      },
+      accept: function(a, b) {
+        var ret = { done: function(c) { ret.cb = c; }};
+        freedom.tran.accept(a, b, function(x) {
+          ret.cb(x);
+        });
+        return ret;
+      }
+    }
+    fdom.tp = x;
+    return x;
   },
   on: function(a, f) {
     freedom['on' + a] = f;
